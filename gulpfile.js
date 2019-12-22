8a7043c51989f7757df2ba0dc71a6d4096a5474e
jihyunKwon <jihyunkwon29@gmail.com>
2019-12-22 17:15:45 +0900

    Initialize es-lint with gulp-lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 const gulp = require('gulp');
-<<<<<<< HEAD
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
 
 gulp.task('default', async function() {
+    //eslint를 시작합니다
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
+    //노드 소스
     gulp.src('es6/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest("dist"));
+    //브라우저 소스
     gulp.src("public/es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("public/dist"));
-=======
-//걸프 의존성을 여기 쓴다
 
-gulp.task('default', function()  {
-//걸프 작업을 여기 쓴다
->>>>>>> b697a89d738b064ec633d6963bb33167699d56df
+
 });
